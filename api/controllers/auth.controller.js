958c9b025df5c41bf43e183952ee4b4c21890fc3
Rashed <rashedulhoqueceo@gmail.com>
2024-04-27 23:43:27 +0600

    added prisma schima for user register

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body;
+
+
+    try{
 
     // HASH THE PASSWORD
 
@@ -10,6 +14,21 @@ export const register = async (req, res) => {
 
 
     // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    console.log(newUser);
+
+    res.status(201).json({message: "User created successfully"})
+}catch(err){
+    console.log(err);
+    res.status(500).json({message: "Failed to create user!"})
+}
 
 }
 
