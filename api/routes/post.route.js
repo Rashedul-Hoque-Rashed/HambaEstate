1c44e483c868a856e46e3bf697b449caec620603
Rashed <rashedulhoqueceo@gmail.com>
2024-04-27 20:56:53 +0600

    create auth route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    console.log('router worked');
-});
+// router.get("/test", (req, res) => {
+//     console.log('router worked');
+// });
+// router.post("/test", (req, res) => {
+//     console.log('router worked');
+// });
+// router.put("/test", (req, res) => {
+//     console.log('router worked');
+// });
+// router.delete("/test", (req, res) => {
+//     console.log('router worked');
+// });
 
 export default router;
