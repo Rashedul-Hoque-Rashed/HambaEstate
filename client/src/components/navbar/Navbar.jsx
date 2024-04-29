8c7b6bed788e961bd46e8191a8a8d19e2f2ff646
Rashed <rashedulhoqueceo@gmail.com>
2024-04-29 19:55:24 +0600

    make navbar responsive

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./Navbar.scss"
 
 function Navbar() {
+    const [open, setOpen] = useState(false);
+    console.log(open);
     return (
         <nav>
             <div className="left">
@@ -14,8 +17,19 @@ function Navbar() {
                 <a href="/">Agents</a>
             </div>
             <div className="right">
-            <a href="/">Sing in</a>
+                <a href="/">Sing in</a>
                 <a href="/" className="register">Sing up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sing in</a>
+                    <a href="/">Sing up</a>
+                </div>
             </div>
         </nav>
     )
