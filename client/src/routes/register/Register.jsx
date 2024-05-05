4ea8f7e994f1bb7b4fbc5470c8c460b96e6a119a
Rashed <rashedulhoqueceo@gmail.com>
2024-05-05 23:49:32 +0600

    create action for login and register

--- client/src/routes/register/Register.jsx
+++ client/src/routes/register/Register.jsx
@@ -1,16 +1,52 @@
 import "./Register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
+
 
 function Register() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("")
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.response?.data?.message);
+    } 
+    finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div className="register">
+    <div className="registerPage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="email" type="text" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <input type="text" name="username" id="username" placeholder="Username" />
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <input type="password" name="password" id="password" placeholder="Password" />
+          
+          
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
