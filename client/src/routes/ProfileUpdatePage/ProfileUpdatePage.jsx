7b2cbc28399c7eb4cfed32d5dfca4c58dd04a23f
Rashed <rashedulhoqueceo@gmail.com>
2024-05-09 23:07:42 +0600

    create user avatar  update function on update profile page

--- client/src/routes/ProfileUpdatePage/ProfileUpdatePage.jsx
+++ client/src/routes/ProfileUpdatePage/ProfileUpdatePage.jsx
@@ -1,27 +1,37 @@
 import { useContext, useState } from "react";
 import "./ProfileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
-import apiRequest from './../../lib/apiRequest';
-import { useNavigate } from 'react-router-dom';
+import apiRequest from "../../lib/apiRequest";
+import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
 
 function ProfileUpdatePage() {
-  const [error, setError] = useState("");
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [avatar, setAvatar] = useState([]);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
+
     const { username, email, password } = Object.fromEntries(formData);
+
     try {
-      const res = await apiRequest.put(`/users/${currentUser.id}`, { username, email, password });
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar:avatar[0]
+      });
       updateUser(res.data);
-      navigate('/profile')
+      navigate("/profile");
     } catch (err) {
       console.log(err);
-      setError(err.response.data.message)
+      setError(err.response.data.message);
     }
-  }
+  };
 
   return (
     <div className="profileUpdatePage">
@@ -34,7 +44,7 @@ function ProfileUpdatePage() {
               id="username"
               name="username"
               type="text"
-              defaultValue={currentUser?.username}
+              defaultValue={currentUser.username}
             />
           </div>
           <div className="item">
@@ -43,19 +53,29 @@ function ProfileUpdatePage() {
               id="email"
               name="email"
               type="email"
-              defaultValue={currentUser?.email}
+              defaultValue={currentUser.email}
             />
           </div>
           <div className="item">
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
-          <button type="submit">Update</button>
-          {error && <span>{error}</span>}
+          <button>Update</button>
+          {error && <span>error</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser?.avatar || '/noavatar.jpg'} alt="" className="avatar" />
+        <img src={avatar[0] || currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "rashedul",
+            uploadPreset: "hambaEstate",
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: "avatars",
+          }}
+          setState={setAvatar}
+        />
       </div>
     </div>
   );
