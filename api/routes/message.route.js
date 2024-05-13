2082ae54f5f31fc107e314ebd0a4eecbd38c72af
Rashed <rashedulhoqueceo@gmail.com>
2024-05-13 23:08:30 +0600

    create chat route and controller

---
+++ api/routes/message.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteUsers, getUser, getUsers, updateUsers, savePost, profilePosts } from "../controllers/user.controller.js";
+import { verifyToken } from '../middleware/verifyToken.js';
+
+
+const router = express.Router();
+
+router.get("/", getUsers);
+// router.get("/search/:id", verifyToken, getUser);
+router.put("/:id", verifyToken,  updateUsers);
+router.delete("/:id", verifyToken,  deleteUsers);
+router.post("/save", verifyToken,  savePost);
+router.get("/profilePosts", verifyToken,  profilePosts);
+
+export default router;
