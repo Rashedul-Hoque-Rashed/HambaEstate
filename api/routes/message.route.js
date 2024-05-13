ddaa5cd71baa8a33e0d908f5de6415aa3c88dbd5
Rashed <rashedulhoqueceo@gmail.com>
2024-05-13 23:22:40 +0600

    create message route and controller

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,15 +1,12 @@
 import express from "express";
-import { deleteUsers, getUser, getUsers, updateUsers, savePost, profilePosts } from "../controllers/user.controller.js";
-import { verifyToken } from '../middleware/verifyToken.js';
-
+import {
+  addMessage
+} from "../controllers/message.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-// router.get("/search/:id", verifyToken, getUser);
-router.put("/:id", verifyToken,  updateUsers);
-router.delete("/:id", verifyToken,  deleteUsers);
-router.post("/save", verifyToken,  savePost);
-router.get("/profilePosts", verifyToken,  profilePosts);
+
+router.post("/:chatId", verifyToken, addMessage);
 
 export default router;
